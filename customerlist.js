e5a402dd7358bb6fa09df6fcae4be8f2a50fda39
Rob Ashton <robashton@codeofrob.com>
2013-10-23 14:50:01 +0100

    Customers can be clicked

--- customerlist.js
+++ customerlist.js
@@ -1,16 +1,23 @@
 var templates = require('./templates')
   , mustache = require('mustache')
+  , Delegate = require('dom-delegate')
+  , dope = require('dope')
 
 function CustomerList(element, customers) {
   this.element = element
   this.customers = customers
   this.render()
+  this.delegate = new Delegate(this.element)
+  this.delegate.on('click', '.customer', this.onCustomerClicked.bind(this))
 }
 
 CustomerList.prototype = {
   render: function() {
     var self = this
     this.element.innerHTML = mustache.render(templates.customers, this)
+  },
+  onCustomerClicked: function(e, row) {
+    alert('Clicked ' + dope.dataset(row).customer)
   },
   detach: function() {
 
--- templates.js
+++ templates.js
@@ -1,7 +1,7 @@
 module.exports = {
   customers: ["<table>",
               "{{#customers}}",
-                '<tr class="customer"><td>{{name}}</td><td>{{bank}}</td></tr>',
+              '<tr class="customer" data-customer="{{name}}"><td>{{name}}</td><td>{{bank}}</td></tr>',
               "{{/customers}}",
               "</table>"].join("\n"),
 }
